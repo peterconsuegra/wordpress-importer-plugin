f70e23b58497bf8937e3c9219e8faf23642d15cc
Pedro Consuegra <pedroconsuegrat@gmail.com>
2022-02-12 14:18:14 -0500

    fix create title

--- src/views/create.blade.php
+++ src/views/create.blade.php
@@ -12,7 +12,7 @@
 	<div class="row">
 		<div class="col-md-12">
 				<div class="page-header">
-						<h3>Import WordPress Instancexx</h3>
+						<h3>Import WordPress Instance</h3>
 	
 				</div>
 		</div>
