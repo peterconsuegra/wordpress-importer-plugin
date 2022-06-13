c1b24d0cd315799db54fc85e470164cc854cfd2c
Pedro Consuegra <pedroconsuegrat@gmail.com>
2022-06-13 17:16:37 -0500

    kingston changes in plugin

--- src/Http/WordPressImporterController.php
+++ src/Http/WordPressImporterController.php
@@ -50,71 +50,52 @@ class WordPressImporterController extends Controller
 	
 	public function store(Request $request)
 	{
-
-		$pete_options = new PeteOption();
-		$user = Auth::user();
-		$fields_to_validator = $request->all();
 		
-		$site = new Site();
-		$site->output = "";
-		$site->user_id = $user->id;
-		$site->app_name = $request->input("app_name");
-		$site->action_name = "Import";
-		$site->to_import_project = $request->input("to_import_project");
-		$site->user_id = $user->id;
-		$site->url = $request->input("url");
-		$site->set_project_name($site->url);
-		$site->big_file_route = $request->input("big_file_route");
+		$current_user = Auth::user();
+		$request_array = $request->all();
+		$new_site = new Site();
+		$new_site->set_url($request->input("url"));
+		$new_site->set_project_name($new_site->url);
 		
-		$app_root = $pete_options->get_meta_value('app_root');
-		if($pete_options->get_meta_value('domain_template')){
-	
-			$site->url = $site->url . "." . $pete_options->get_meta_value('domain_template');
-		}
-		
-		$fields_to_validator["name"] = $site->name;
-		$validator = Validator::make($fields_to_validator, [
-			'name' =>  array('required', 'regex:/^[a-zA-Z0-9-_]+$/','unique:sites'),
-			'url' => 'required|unique:sites',
-		]);
-		
-     	if ($validator->fails()) {
-			
-	        return redirect('import_wordpress')
-	        		->withErrors($validator)
-	        			->withInput();
-     	 }
-		
-		if($request->file('filem')!= ""){
-			
+		$errors = $new_site->can_this_site_be_create();
+		if($errors["error"]){
+			 $result = array_merge($errors, $request_array);
+			 return redirect('/import_wordpress')->withErrors("Forbidden project name");
+		}else{
+				
 			/*
-			$size = $request->file('filem')->getSize();	
-			
-			$file_size = number_format($size / 1048576,2);
-			Log::info($file_size.' MB');	
-		
-			if($file_size > 450){
-				return Redirect::to('/import_wordpress')->withErrors(['msg' => 'This file exceeds the size (450MB) limit for an http post request please try the option: File path for large files (Optional)']);
-			}
+			1) Create Snapshot
+			2) Import Snapshot
+			3) Delete Snapshot
 			*/
+			$new_site->big_file_route = $request->input("big_file_route");
 			
-			$file = $request->file('filem');
-	        // SET UPLOAD PATH
-	        $destinationPath = 'uploads';
-	         // GET THE FILE EXTENSION
-	        $extension = $file->getClientOriginalExtension();
-	         // RENAME THE UPLOAD WITH RANDOM NUMBER
-	        $fileName = rand(11111, 99999) . '.' . $extension;
-	         // MOVE THE UPLOADED FILES TO THE DESTINATION DIRECTORY
-	        $upload_success = $file->move($destinationPath, $fileName);
-			$site->zip_file_url = $fileName;
-		
+			if($request->file('filem')!= ""){
+				$file = $request->file('filem');
+		        // SET UPLOAD PATH
+		        $destinationPath = 'uploads';
+		         // GET THE FILE EXTENSION
+		        $extension = $file->getClientOriginalExtension();
+		         // RENAME THE UPLOAD WITH RANDOM NUMBER
+		        $fileName = rand(11111, 99999) . '.' . $extension;
+		         // MOVE THE UPLOADED FILES TO THE DESTINATION DIRECTORY
+		        $upload_success = $file->move($destinationPath, $fileName);
+				$new_site->zip_file_url = $fileName;
+			}
+			
+			$current_user = Auth::user();
+			
+			$import_params = array_merge(
+			["template" => "none",
+			"action_name" => "Import", 
+			"user_id" => $current_user->id, 
+			],$request_array);
+			
+			$new_site->import_wordpress($import_params);
+	
+			return Redirect::to('/sites/'.$new_site->id .'/edit' .'?success=' . 'true');
 		}
 		
-	
-		$site->import_wordpress();
-			
-		return Redirect::to('/sites/'.$site->id .'/edit' .'?success=' . 'true');
 	}	
 	
 }
