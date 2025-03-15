80d516afe6dad01cc34259025520c92611aa4485
Pedro <pedroconsuegrat@gmail.com>
2025-03-15 17:01:49 +0000

    llll

--- src/views/create.blade.php
+++ src/views/create.blade.php
@@ -14,7 +14,7 @@
 				<div class="page-header">
 						<h3>Import WordPress Instance</h3>
 						
-						<i>Use PHP 8.0 or 8.1 with WordPress 5.9 or 6.0 for compatibility and better performance</i>
+						<label>Use PHP 8.0 or 8.1 with WordPress 5.9 or 6.0 for compatibility and better performance.</label>
 	
 				</div>
 		</div>
@@ -65,20 +65,26 @@
 	
 	<div class="row">
 		<div class="col-md-12">
-													
+									
+									
+			<div class="form-group" id="zip_file_url_div" >
+				<label for="zip_file_url-field">WordPress Pete file</label>
+				<input type="file" id="filem" name="filem">
+			</div>
+						
+			<div id="big_file_container"><input type="checkbox" id="big_file" name="url_template"  value="true"> &nbsp; File path for large files (Optional)</div>
+							
+			
+			
 			@if($pete_options->get_meta_value('os_distribution') == "docker")		
-				<p><i id="label_big_file_container">
-					
-					Please copy the WordPress Pete file and paste it into the following directory that's shared with Docker: wordpress-pete-docker/public_html/my_site.tar.gz. After doing so, restart Docker. Note that the file path within Docker will be: /var/www/html/my_site.tar.gz.
-					
-				</i></p>
+				<p><i id="label_big_file_container" style="display: none; font-size:14px">Copy and paste the WordPress Pete file in the path of the volume shared with docker: wordpress-pete-docker/public_html/my_site.tar.gz, after this restart the docker and note that the path in this field will be: /var/www/html/my_site.tar.gz</i></p>
 			@else
 			
-			<p id="label_big_file_container">Enter the path where the WordPress Pete format file is located</p>
+			<p id="label_big_file_container" style="display: none; font-size:14px">Enter the path where the WordPress Pete format file is located</p>
 			
 			@endif
 			
-			<input type="text" id="big_file_route" placeholder="/var/www/html/" name="big_file_route" class="form-control"/>
+			<input type="text" id="big_file_route" name="big_file_route" style="display: none;" class="form-control"/>
 				
 			<br/>
 					
@@ -96,7 +102,12 @@
 
 <script>
 	
-	
+	$("#big_file").click(function() {
+		
+		//alert("hi big");
+		$("#label_big_file_container").toggle();
+		$("#big_file_route").toggle();
+	});
 	
 </script>		
 			
