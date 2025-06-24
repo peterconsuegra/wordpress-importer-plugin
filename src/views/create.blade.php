0540123af7d36c9a7d60654c22fa7bb6badbf590
Pedro Consuegra <pedroconsuegrat@gmail.com>
2025-06-24 11:27:08 +0000

    remove file field logic

--- src/views/create.blade.php
+++ src/views/create.blade.php
@@ -30,10 +30,6 @@
 	<div class="row">
 		<div class="col-md-12">
 
-			<p>Adjust your system's upload capacity. <a href="/phpinfo_panel">[Learn more]</a></p>
-
-			<p>Temporarily disable security to upload your WordPress site. Remember to re-enable it once the process is complete. <a href="/security">[Learn more]</a></p>
-						
 			<div id="url_div">
 				<p>URL</p>
 				<input type="text" id="url-field" name="url" class="inline_class url_wordpress_laravel"/>
@@ -67,24 +63,8 @@
 	<div class="row">
 		<div class="col-md-12">
 									
-									
-			<div class="form-group" id="zip_file_url_div" >
-				<label for="zip_file_url-field">WordPress Pete file</label>
-				<input type="file" id="filem" name="filem">
-			</div>
-						
-			<div id="big_file_container"><input type="checkbox" id="big_file" name="url_template"  value="true"> &nbsp; Optional: Specify File Path for Large Files</div>
-							
-			
-			
-			@if($pete_options->get_meta_value('os_distribution') == "docker")		
-				<p><i id="label_big_file_container" style="display: none; font-size:14px">Copy and paste the WordPress Pete file in the path of the volume shared with docker: wordpress-pete-docker/public_html/my_site.tar.gz, after this restart the docker and note that the path in this field will be: /var/www/html/my_site.tar.gz</i></p>
-			@else
-			
-			
-			@endif
-			
-			<input type="text" id="big_file_route" placeholder="/var/www/html/mysite.tar.gz" name="big_file_route" style="display: none;" class="form-control"/>
+			<p>File path</p>				
+			<input type="text" id="big_file_route" placeholder="/var/www/html/mysite.tar.gz" name="big_file_route" class="form-control"/>
 				
 			<br/>
 					
